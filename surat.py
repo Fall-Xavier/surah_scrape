be46294c4962adbe50a17e790f4aac7aaf721635
Fall Xavier <84975017+Fall-Xavier@users.noreply.github.com>
2022-11-12 13:35:52 +0700

    Update surat.py

--- surat.py
+++ surat.py
@@ -1,26 +1,77 @@
 import re,requests,bs4
 from bs4 import BeautifulSoup as parser
 ses=requests.Session()
-no,surat,nama_surat=0,[],[]
 
-url = parser(ses.get("https://litequran.net/").text,"html.parser")
-for data in url.find_all("a",href=True):
-	nama_surat.append(data.text)
-	if "https://Litequran.net/" in data.get("href"):
-		pass
-	else:
-		if "Kebijakan Privasi" in data.text:
-			pass
-		else:
-			no +=1
-			print(f"{no}. {data.text}")
-			surat.append(data.get("href"))
+class ScrapeSurah:
+	
+	def __init__(self):
+		self.no,self.num = 0,0
+		self.user_surah = []
+		self.nama_surah = []
+		self.ayat = []
+		self.surah_fatihah = []
+		self.angka = []
+		self.url = "https://litequran.net/"
+		
+	def Get_Kelipatan(self,angka):
+		kelipatan = angka
+		while kelipatan < 9999:
+			self.angka.append(kelipatan)
+			kelipatan = kelipatan + angka
+	
+	def Menu(self):
+		url = parser(ses.get(self.url).text,"html.parser")
+		for data in url.find_all("a",href=True):
+			self.nama_surah.append(data.text)
+			if "https://Litequran.net/" in data.get("href"):
+				pass
+			else:
+				if "Kebijakan Privasi" in data.text:
+					pass
+				else:
+					self.no +=1
+					print(f"{self.no}. {data.text}")
+					self.user_surah.append(data.get("href"))
+		ask = input("\n masukan nomer : ")
+		self.Get_Surah(self.user_surah[int(ask)-1])
+		
+	def Get_Surah(self,nama):
+		url = parser(ses.get(self.url+nama).text,"html.parser")
+		if nama == "al-fatihah":
+			self.Al_Fatihah(url)
+		self.Get_Kelipatan(4)
+		for data in url.find_all("p"):
+			if len(self.ayat) in self.angka:
+				self.ayat.append("<>")
+			self.ayat.append(f"{data.text}|")
+		for isi in "".join(self.ayat).split("<>"):
+			self.num +=1
+			nanya = isi.split("|")
+			print(f"""
+Ayat Ke {self.num}
+{nanya[0]}
+{nanya[1]}
 
-ask = input("\n masukan nomer : ")
-urel = parser(ses.get(f"https://litequran.net/{surat[int(ask)-1]}").text,"html.parser")
-for det in urel.find_all("p",{"class": ["arabic","translate"]}):
-	print(det.text)
+Artinya : {nanya[2]}""")
+		exit()
+			
+	def Al_Fatihah(self,url):
+		self.Get_Kelipatan(4)
+		for data in url.find_all("p"):
+			if len(self.surah_fatihah) in self.angka:
+				#self.angka.clear()
+				self.surah_fatihah.append("<>")
+				#self.Get_Kelipatan(4)
+			self.surah_fatihah.append(f"{data.text}|")
+		for isi in "".join(self.surah_fatihah).split("<>"):
+			self.num +=1
+			nanya = isi.split("|")
+			print(f"""
+Ayat Ke {self.num}
+{nanya[0]}
+{nanya[1]}
+
+Artinya : {nanya[2]}""")
+		exit()
 		
-		
-		
-		
+ScrapeSurah().Menu()
